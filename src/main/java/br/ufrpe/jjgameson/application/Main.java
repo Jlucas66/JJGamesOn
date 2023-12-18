480b9a1b4f3b5137aecbab1707b3a21c3b4cec89
Artur Guimarães <51428160+Guimaaaas@users.noreply.github.com>
2023-12-18 11:08:59 -0300

    Update main

--- src/main/java/br/ufrpe/jjgameson/application/Main.java
+++ src/main/java/br/ufrpe/jjgameson/application/Main.java
@@ -1,7 +1,12 @@
 package br.ufrpe.jjgameson.application;
 
+import br.ufrpe.jjgameson.entidades.Pessoa;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-
+            Pessoa admin1 = new Pessoa("João", "joaoemail@gmail.com", "senha123", LocalDate.of(1999,12,12),true);
+            Pessoa cliente1 = new Pessoa("Vagner", "vagneremail@gmail.com", "coxinha123", LocalDate.of(2002,5,15),false);
         }
 }
