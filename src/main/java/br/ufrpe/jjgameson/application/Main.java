7340d91b61a1bcdb4365ca826c16e02c94946f53
Artur Guimarães <51428160+Guimaaaas@users.noreply.github.com>
2023-12-18 11:16:02 -0300

    Update main

--- src/main/java/br/ufrpe/jjgameson/application/Main.java
+++ src/main/java/br/ufrpe/jjgameson/application/Main.java
@@ -1,12 +1,29 @@
 package br.ufrpe.jjgameson.application;
 
+import br.ufrpe.jjgameson.dados.*;
 import br.ufrpe.jjgameson.entidades.Pessoa;
 
 import java.time.LocalDate;
 
 public class Main {
+
     public static void main(String[] args) {
-            Pessoa admin1 = new Pessoa("João", "joaoemail@gmail.com", "senha123", LocalDate.of(1999,12,12),true);
-            Pessoa cliente1 = new Pessoa("Vagner", "vagneremail@gmail.com", "coxinha123", LocalDate.of(2002,5,15),false);
+
+        // Instanciando Repositorios:
+        RepositorioAdmin repositorioAdmin = new RepositorioAdmin();
+        RepositorioCliente repositorioCliente = new RepositorioCliente();
+        RepositorioCodigo repositorioCodigo = new RepositorioCodigo();
+        RepositorioAvaliacao repositorioAvaliacao = new RepositorioAvaliacao();
+        RepositorioJogo repositorioJogo = new RepositorioJogo();
+        RepositorioPromocao repositorioPromocao = new RepositorioPromocao();
+        RepositorioVenda repositorioVenda = new RepositorioVenda();
+
+        // Gerando Pessoas
+        Pessoa admin1 = new Pessoa("João", "joaoemail@gmail.com", "senha123", LocalDate.of(1999,12,12),true);
+        Pessoa cliente1 = new Pessoa("Vagner", "vagneremail@gmail.com", "coxinha123", LocalDate.of(2002,5,15),false);
+
+        // Inserindo no repositorio
+        repositorioAdmin.inserir(admin1);
+        repositorioCliente.inserirCliente(cliente1);
         }
 }
