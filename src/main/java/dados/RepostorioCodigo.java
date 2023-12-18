a1c2ddcec7c7206183ff13f18feaf91e3f717545
Artur Guimarães <51428160+Guimaaaas@users.noreply.github.com>
2023-12-18 03:35:07 -0300

    Update RepositorioCodigo.java
    implementando interface

--- src/main/java/dados/RepostorioCodigo.java
+++ src/main/java/dados/RepostorioCodigo.java
@@ -1,5 +1,18 @@
 package dados;
 
-public class RepostorioCodigo {
-    
+import entidades.Codigo;
+
+import java.util.List;
+
+public class RepostorioCodigo implements IRepostorioCodigo{
+
+    @Override
+    public void inserir(Codigo codigo) {
+        
+    }
+
+    @Override
+    public List<Codigo> listar() {
+        return null;
+    }
 }
