394c48a4e32f3f715b021effebd140624d7e5e2c
Artur Guimarães <51428160+Guimaaaas@users.noreply.github.com>
2023-12-18 05:21:24 -0300

    update vendas

--- src/main/java/dados/IRepositorioVenda.java
+++ src/main/java/dados/IRepositorioVenda.java
@@ -9,6 +9,6 @@ import java.util.List;
 public interface IRepositorioVenda {
     public void inserir (Venda venda);
     public List<Venda> listar();
-    public void atualizar (Venda venda);
-    public void excluir(LocalDateTime dtVenda);
+    public void atualizar (Venda vendaAntiga, Venda vendaNova);
+    public void excluir(Venda vendaParaExcluir);
 }
--- src/main/java/dados/RepositorioVenda.java
+++ src/main/java/dados/RepositorioVenda.java
@@ -1,29 +1,50 @@
 package dados;
 
+import entidades.Avaliacao;
 import entidades.Venda;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 public class RepositorioVenda implements IRepositorioVenda{
 
+    private ArrayList<Venda> vendas;
+
+    public RepositorioVenda() {
+        vendas = new ArrayList<>();
+    }
     @Override
     public void inserir(Venda venda) {
-        
+        if(venda != null){
+            vendas.add(venda);
+        }
     }
 
     @Override
     public List<Venda> listar() {
-        return null;
+        return vendas;
     }
 
     @Override
-    public void atualizar(Venda venda) {
-
+    public void atualizar(Venda vendaAntiga, Venda vendaNova) {
+        for (Venda venda : vendas) {
+            if (venda.equals(vendaAntiga)) {
+                int index = vendas.indexOf(venda);
+                vendas.set(index, vendaNova);
+                break;
+            }
+        }
     }
 
     @Override
-    public void excluir(LocalDateTime dtVenda) {
+    public void excluir(Venda vendaParaExcluir) {
+        for (Venda venda : vendas) {
+            if (venda.equals(vendaParaExcluir)) {
+                vendas.remove(venda);
+                break;
+            }
+        }
 
     }
 }
