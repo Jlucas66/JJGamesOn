379f2b676b71d2a4872ffa272f9fca2ec1b162b6
Artur Guimarães <51428160+Guimaaaas@users.noreply.github.com>
2023-12-18 03:44:25 -0300

    Update repositorio promocao

--- src/main/java/dados/IRepositorioPromocao.java
+++ src/main/java/dados/IRepositorioPromocao.java
@@ -1,5 +1,13 @@
 package dados;
 
+import entidades.Pessoa;
+import entidades.Promocao;
+
+import java.util.List;
+
 public interface IRepositorioPromocao {
-    
+    public void inserir (Promocao promocao);
+    public List<Promocao> listar();
+    public void atualizar (Promocao promocao);
+    public void excluir(int idPromocao);
 }
--- src/main/java/dados/RepositorioPromocao.java
+++ src/main/java/dados/RepositorioPromocao.java
@@ -1,5 +1,28 @@
 package dados;
 
-public class RepositorioPromocao {
-    
+import entidades.Promocao;
+
+import java.util.List;
+
+public class RepositorioPromocao implements IRepositorioPromocao{
+
+    @Override
+    public void inserir(Promocao promocao) {
+        
+    }
+
+    @Override
+    public List<Promocao> listar() {
+        return null;
+    }
+
+    @Override
+    public void atualizar(Promocao promocao) {
+
+    }
+
+    @Override
+    public void excluir(int idPromocao) {
+
+    }
 }
