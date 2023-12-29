399ffe7d5cf3188876d9dd552a617817c3c74e55
Alexander Fábio da Silva <alexanderfabio903@gmail.com>
2023-12-29 02:46:32 -0300

    Tela cadastro cliente e controlador

---
+++ src/main/java/br/ufrpe/jjgameson/gui/TelaCadastroClienteControlador.java
@@ -0,0 +1,47 @@
+package br.ufrpe.jjgameson.gui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+public class TelaCadastroClienteControlador {
+
+    @FXML
+    private Button botaoRealizarCadastro;
+
+    @FXML
+    private Button botaoVoltar;
+
+    @FXML
+    private ImageView capaCadastro;
+
+    @FXML
+    private DatePicker dataNascimentoCadastro;
+
+    @FXML
+    private TextField emailCadastro;
+
+    @FXML
+    private ImageView logoCadastro;
+
+    @FXML
+    private TextField nomeCadastro;
+
+    @FXML
+    private PasswordField senhaCadastro;
+
+    @FXML
+    void btnCadastroClienteRealizarCadastro(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnCadastroClienteVoltar(ActionEvent event) {
+
+    }
+
+}
