a2bfa407919ef0cf28af7d9914dd10cdd778f6b7
Alexander Fábio da Silva <alexanderfabio903@gmail.com>
2023-12-29 02:51:27 -0300

    Tela login adm e controlador

---
+++ src/main/java/br/ufrpe/jjgameson/gui/TelaLoginADMControlador.java
@@ -0,0 +1,37 @@
+package br.ufrpe.jjgameson.gui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+public class TelaLoginADMControlador {
+
+    @FXML
+    private Button botaoEntrarComoADM;
+
+    @FXML
+    private Button botaoVoltar;
+
+    @FXML
+    private TextField emailLoginADM;
+
+    @FXML
+    private ImageView logoLoginADM;
+
+    @FXML
+    private TextField senhaLoginADM;
+
+    @FXML
+    void btnLoginADMEntrarComoADM(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnLoginADMVoltar(ActionEvent event) {
+
+    }
+
+}
+
