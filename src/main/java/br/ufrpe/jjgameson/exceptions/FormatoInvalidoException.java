42e7d07e6deab37a6173b030299e77103538b885
Anthony <anthony_efraim@hotmail.com>
2024-01-05 11:22:56 -0300

    Regras de negocio: ControladorCodigo

---
+++ src/main/java/br/ufrpe/jjgameson/exceptions/FormatoInvalidoException.java
@@ -0,0 +1,7 @@
+package br.ufrpe.jjgameson.exceptions;
+
+public class FormatoInvalidoException extends Exception{
+    public FormatoInvalidoException(String mensagem){
+        super(mensagem);
+    }
+}
--- src/main/java/br/ufrpe/jjgameson/negocio/ControladorCodigo.java
+++ src/main/java/br/ufrpe/jjgameson/negocio/ControladorCodigo.java
@@ -2,6 +2,9 @@ package br.ufrpe.jjgameson.negocio;
 
 import br.ufrpe.jjgameson.dados.IRepositorioCodigo;
 import br.ufrpe.jjgameson.dados.RepositorioCodigo;
+import br.ufrpe.jjgameson.exceptions.ElementoDuplicadoException;
+import br.ufrpe.jjgameson.exceptions.ElementoNuloException;
+import br.ufrpe.jjgameson.exceptions.FormatoInvalidoException;
 
 public class ControladorCodigo {
 
@@ -12,18 +15,29 @@ public class ControladorCodigo {
         repositorioCodigo = RepositorioCodigo.getInstance();
     }
 
-    public static ControladorCodigo getInstance(){
-        if(instance == null){
+    public static ControladorCodigo getInstance() {
+        if (instance == null) {
             instance = new ControladorCodigo();
         }
         return instance;
     }
-
-    public void inserir(String codigo){
+    public void inserir(String codigo) throws ElementoNuloException, FormatoInvalidoException, ElementoDuplicadoException {
+        if(codigo == null || codigo.isEmpty()){
+            throw new ElementoNuloException("O código não pode ser nulo ou vazio.");
+        }
+        if(codigo.length() != 10){
+            throw new FormatoInvalidoException("O código deve ter 10 caracteres.");
+        }
+        if(!codigo.matches("[a-zA-Z0-9]+")){
+            throw new FormatoInvalidoException("O código deve conter apenas letras e números.");
+        }
+        if(repositorioCodigo.listar().contains(codigo)){
+            throw new ElementoDuplicadoException("O código já foi cadastrado.");
+        }
         repositorioCodigo.inserir(codigo);
     }
 
-    public void listar(){
+    public void listar() {
         repositorioCodigo.listar();
     }
 }
