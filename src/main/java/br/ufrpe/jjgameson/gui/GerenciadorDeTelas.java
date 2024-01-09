a39a51cbdf6dd9cc92412868c6ade150c8b00d1d
Alexander Fábio da Silva <alexanderfabio903@gmail.com>
2024-01-09 03:56:22 -0300

    Tela compra finalizada e controlador

--- src/main/java/br/ufrpe/jjgameson/gui/GerenciadorDeTelas.java
+++ src/main/java/br/ufrpe/jjgameson/gui/GerenciadorDeTelas.java
@@ -65,4 +65,13 @@ public class GerenciadorDeTelas {
         stage.setResizable(false);
         stage.show();
     }
+    public static void irParaTelaCompraFinalizada (ActionEvent event) throws IOException {
+        Stage stage;
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("tela_compra_finalizada.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(fxmlLoader.load(), 900, 600));
+        stage.setTitle("Compra finalizada");
+        stage.setResizable(false);
+        stage.show();
+    }
 }
---
+++ src/main/java/br/ufrpe/jjgameson/gui/TelaCompraFinalizadaControlador.java
@@ -0,0 +1,27 @@
+package br.ufrpe.jjgameson.gui;
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.image.ImageView;
+
+    public class TelaCompraFinalizadaControlador {
+
+        @FXML
+        private Button botaoVoltar;
+
+        @FXML
+        private TextArea codigosDeAcessoCompraFinalizada;
+
+        @FXML
+        private ImageView logoCompraFinalizada;
+
+        @FXML
+        void btnVoltarCompraFinalizada(ActionEvent event) {
+
+        }
+
+    }
+
