1b5a465da6adff46f0741c8f6e50f471d8ce9910
Alexander Fábio da Silva <alexanderfabio903@gmail.com>
2024-01-16 17:41:21 -0300

    Card da tela principal cliente e controlador

---
+++ src/main/java/br/ufrpe/jjgameson/gui/CardTelaPrincipalClienteControlador.java
@@ -0,0 +1,34 @@
+package br.ufrpe.jjgameson.gui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+
+import java.io.IOException;
+
+public class CardTelaPrincipalClienteControlador {
+        @FXML
+        private ImageView capa;
+
+        @FXML
+        private Label dev;
+
+        @FXML
+        private Hyperlink nome;
+
+        @FXML
+        private Pane pane;
+
+        @FXML
+        private Label preco;
+
+        @FXML
+        void nomeJogoCard(ActionEvent event) throws IOException {
+
+        }
+
+    }
+
