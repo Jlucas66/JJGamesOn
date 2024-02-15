03f753e6b22a51ba30d1c4ded85dafcfc590ac2e
Artur Guimarães <51428160+Guimaaaas@users.noreply.github.com>
2024-02-15 11:33:37 -0300

    Update TelaCadastroClienteControlador.java
    fazendo cadastro de um cliente

--- src/main/java/br/ufrpe/jjgameson/gui/TelaCadastroClienteControlador.java
+++ src/main/java/br/ufrpe/jjgameson/gui/TelaCadastroClienteControlador.java
@@ -1,25 +1,31 @@
 package br.ufrpe.jjgameson.gui;
 
 import br.ufrpe.jjgameson.dados.RepositorioCliente;
+import br.ufrpe.jjgameson.entidades.FaixaEtaria;
+import br.ufrpe.jjgameson.entidades.Jogo;
 import br.ufrpe.jjgameson.entidades.Pessoa;
+import br.ufrpe.jjgameson.exceptions.*;
 import br.ufrpe.jjgameson.negocio.Fachada;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
 
 import java.io.IOException;
+import java.net.URL;
 import java.sql.SQLData;
 import java.time.LocalDate;
 import java.util.Date;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
-public class TelaCadastroClienteControlador {
+public class TelaCadastroClienteControlador implements Initializable {
 
     public TelaCadastroClienteControlador() {
     }
+
+    LocalDate dataNascimento;
 
     @FXML
     private Button botaoRealizarCadastro;
@@ -48,6 +54,26 @@ public class TelaCadastroClienteControlador {
     @FXML
     public void btnCadastroClienteRealizarCadastro(ActionEvent event) throws IOException {
 
+        String nomeCadastroString = nomeCadastro.getText();
+        String emailCadastroString = emailCadastro.getText();
+        String senhaCadastroString = senhaCadastro.getText();
+
+        Pessoa cliente = new Pessoa(nomeCadastroString,emailCadastroString,senhaCadastroString,dataNascimento,false);
+        try {
+            Fachada.getInstance().inserirCliente(cliente);
+            GerenciadorDeTelas.exibirAlertaMensagem("Conta Criada!", "Sua conta foi criada com sucesso com sucesso!");
+            GerenciadorDeTelas.irParaTelaLoginCliente(event);
+        } catch (ElementoInvalidoException e) {
+            throw new RuntimeException(e);
+        } catch (ElementoNuloException e) {
+            throw new RuntimeException(e);
+        } catch (SenhaFracaException e) {
+            throw new RuntimeException(e);
+        } catch (ElementoDuplicadoException e) {
+            throw new RuntimeException(e);
+        } catch (AcessoInvalidoException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @FXML
@@ -55,4 +81,12 @@ public class TelaCadastroClienteControlador {
         GerenciadorDeTelas.irParaTelaLoginCliente(event);
     }
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        // Adiciona um listener para a propriedade value do DatePicker
+        dataNascimentoCadastro.valueProperty().addListener((observable, oldValue, newValue) -> {
+            // Atualiza a variável aData com o valor selecionado no DatePicker
+            dataNascimento = newValue;
+        });
+    }
 }
