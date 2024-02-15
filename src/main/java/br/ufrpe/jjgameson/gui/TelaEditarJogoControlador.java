98bafd0318ea1afa51f5d121f7669b90424fce45
Artur Guimarães <51428160+Guimaaaas@users.noreply.github.com>
2024-02-14 22:27:23 -0300

    Update TelaEditarJogoControlador.java
    criando alertas de edição de jogo

--- src/main/java/br/ufrpe/jjgameson/gui/TelaEditarJogoControlador.java
+++ src/main/java/br/ufrpe/jjgameson/gui/TelaEditarJogoControlador.java
@@ -3,10 +3,7 @@ package br.ufrpe.jjgameson.gui;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
 
 import java.io.IOException;
@@ -51,7 +48,21 @@ public class TelaEditarJogoControlador {
 
         @FXML
         void btnEditarEditarJogo(ActionEvent event) throws IOException {
-                
+
+
+                // Caso tenha dado algum erro
+                Alert alert = new Alert(Alert.AlertType.WARNING);
+                alert.setTitle("Erro");
+                alert.setHeaderText("Edição de jogo");
+                alert.setContentText("Não foi possível concluir a edição.");
+                alert.showAndWait();
+
+                // Caso tenha sido possivel editar o jogo
+                Alert alertOk = new Alert(Alert.AlertType.WARNING);
+                alertOk.setTitle("Sucesso");
+                alertOk.setHeaderText("Edição de jogo");
+                alertOk.setContentText("O jogo foi editado com sucesso.");
+                alertOk.showAndWait();
         }
 
         @FXML
