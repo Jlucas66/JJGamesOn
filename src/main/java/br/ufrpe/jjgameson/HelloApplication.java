942ea1821fed88d473a029aa6b1f1c07b8b4dc6d
Artur Guimarães <51428160+Guimaaaas@users.noreply.github.com>
2024-02-15 11:09:50 -0300

    Update HelloApplication.java
    instanciando os repositorios

--- src/main/java/br/ufrpe/jjgameson/HelloApplication.java
+++ src/main/java/br/ufrpe/jjgameson/HelloApplication.java
@@ -1,11 +1,14 @@
 package br.ufrpe.jjgameson;
 
+import br.ufrpe.jjgameson.dados.*;
+import br.ufrpe.jjgameson.entidades.Pessoa;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.time.LocalDate;
 
 public class HelloApplication extends Application {
     @Override
@@ -19,6 +22,16 @@ public class HelloApplication extends Application {
     }
 
     public static void main(String[] args) {
+
+        //Instanciando Repositórios
+        RepositorioAdmin repositorioAdmin = (RepositorioAdmin) RepositorioAdmin.getInstance();
+        RepositorioCliente repositorioCliente = (RepositorioCliente) RepositorioCliente.getInstance();
+        RepositorioCodigo repositorioCodigo = (RepositorioCodigo) RepositorioCodigo.getInstance();
+        RepositorioJogo repositorioJogo = (RepositorioJogo) RepositorioJogo.getInstance();
+        RepositorioVenda repositorioVenda = (RepositorioVenda) RepositorioVenda.getInstance();
+
+        Pessoa pessoaLogada = null;
+
         launch();
     }
 }
